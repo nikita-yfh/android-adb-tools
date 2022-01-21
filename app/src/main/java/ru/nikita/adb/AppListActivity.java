ba9bfee3bf6db5ec56425ec9fb6528065a576309
nikita-yfh <nikita.yfh@gmail.com>
2022-01-21 16:16:32 +0300

    Add ProgressDialog to app list

--- app/src/main/java/ru/nikita/adb/AppListActivity.java
+++ app/src/main/java/ru/nikita/adb/AppListActivity.java
@@ -3,7 +3,9 @@ package ru.nikita.adb;
 import java.util.List;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.widget.BaseAdapter;
 import android.widget.ArrayAdapter;
@@ -19,25 +21,15 @@ import android.content.pm.ApplicationInfo;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 
+
 public class AppListActivity extends ListActivity{
+
+	private static int LOAD_APP_LIST = 1;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-		PackageManager pm = getPackageManager();
-		List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-		apps = new App[packages.size()];
-		for(int i = 0; i < packages.size(); i++){
-			ApplicationInfo info = packages.get(i);
-			apps[i] = new App(
-				pm.getApplicationLabel(info).toString(),
-				info.packageName,
-				info.sourceDir,
-				pm.getApplicationIcon(info)
-			);
-		}
-
-		setListAdapter(new AppListAdapter(this,apps));
+		new AppLoader().execute();
     }
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id){
@@ -47,8 +39,6 @@ public class AppListActivity extends ListActivity{
 		setResult(RESULT_OK, intent);
 		finish();
 	}
-
-	private App[] apps;
 
 	private class App {
 		App(String name, String pkg, String path, Drawable icon){
@@ -62,6 +52,39 @@ public class AppListActivity extends ListActivity{
 		public String path;
 		public Drawable icon;
 	}
+
+	private class AppLoader extends AsyncTask<Void,Void,App[]>{
+		@Override
+		protected void onPreExecute(){
+			pd = new ProgressDialog(AppListActivity.this);
+			pd.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			pd.setMessage("Loading applications...");
+			pd.show();
+		}
+		@Override
+		protected void onPostExecute(App[] apps){
+			setListAdapter(new AppListAdapter(AppListActivity.this, apps));
+			pd.dismiss();
+		}
+		@Override
+		protected App[] doInBackground(Void... voids){
+			PackageManager pm = getPackageManager();
+			List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+			apps = new App[packages.size()];
+			for(int i = 0; i < packages.size(); i++){
+				ApplicationInfo info = packages.get(i);
+				apps[i] = new App(
+					pm.getApplicationLabel(info).toString(),
+					info.packageName,
+					info.sourceDir,
+					pm.getApplicationIcon(info)
+				);
+			}
+			return apps;
+		}
+		private ProgressDialog pd;
+	}
+	
 	private class AppListAdapter extends ArrayAdapter<App>{
 		AppListAdapter(Context context, App[] apps){
 			super(context,R.layout.app_list_item,apps);
@@ -85,12 +108,14 @@ public class AppListActivity extends ListActivity{
 			viewHolder.iconView.setImageDrawable(app.icon);
 			return view;
 		}
-			private class ViewHolder{
+		private class ViewHolder{
 			public ImageView iconView;
 			public TextView nameView;
 			public TextView pkgView;
 		}
 	}
+
+	private App[] apps;
 }
 
 
